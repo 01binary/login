d380e4a66b74a646de43fd639af163506834ca05
valeriy novytskyy <valeriy novytskyy>
2016-09-04 13:50:44 -0700

    New project from template

---
+++ login-popup/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(login_popup.Startup))]
+namespace login_popup
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
